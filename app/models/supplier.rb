d9a874e39950c3d1561998cbca1b93752b428b34
taimour afzal <taimour.rkt@gmail.com>
2022-12-30 13:34:02 +0500

    supplier table is created

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: suppliers
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  address1    :string
+#  address2    :string
+#  country     :string
+#  city        :string
+#  phone       :string
+#  location_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Supplier < ApplicationRecord
+    belongs_to :location
 end
