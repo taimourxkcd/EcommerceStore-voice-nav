3052566c3b74cfcd80e5522917c5751796201f5d
taimour afzal <taimour.rkt@gmail.com>
2022-12-30 13:49:30 +0500

    models edited

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: payments
+#
+#  id                       :integer          not null, primary key
+#  order_id                 :integer
+#  amount                   :decimal(10, 2)
+#  status                   :string(255)
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  transaction_log          :text
+#  transaction_id           :string
+#  currency                 :string(3)
+#  card_number              :string
+#
 class Payment < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_30_083018) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_30_084518) do
   create_table "addresses", force: :cascade do |t|
     t.string "street"
     t.string "street2"
@@ -19,6 +19,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_30_083018) do
     t.string "zip"
     t.float "lat"
     t.float "lng"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -48,6 +54,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_30_083018) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "customer_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.integer "order_id"
+    t.decimal "amount"
+    t.string "status"
+    t.string "transaction_id"
+    t.text "transaction_log"
+    t.string "currency"
+    t.string "card_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "suppliers", force: :cascade do |t|
