1cca58c6bb4140ca4ba87340e02939c966168daa
taimourz <taimour.rkt@gmail.com>
2023-03-01 16:14:16 +0500

    header first part complete

--- app/javascript/src/components/Header.js
+++ app/javascript/src/components/Header.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping over PKR100 & Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+92 3328578135">
+                  +92 3328578135
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
