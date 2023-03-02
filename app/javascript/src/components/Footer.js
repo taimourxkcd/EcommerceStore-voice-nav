4f8351517c84e0ee388f82d3bcc597a12b836c9e
taimourz <taimour.rkt@gmail.com>
2023-03-02 05:14:59 +0500

    footer is complete

--- app/javascript/src/components/Footer.js
+++ app/javascript/src/components/Footer.js
@@ -1,11 +1,119 @@
-import React from 'react'
-import {Outlet} from "react-router-dom"
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram  } from "react-icons/bs";
 
 const Footer = () => {
   return (
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center"></div>
+              <img
+                src={require("../../Public/images/newsletter.png")}
+                alt="newsletter"
+              />
+              <span className="mb-0 text-white h4 ms-4">
+                Sign Up for Newsletter
+              </span>
+            </div>
+            <div className="col-7">
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control py-1"
+                  placeholder="Your Email Addresss"
+                  aria-label="Your Email Addresss"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                  Hno : 277 Nadir Street Rawalpindi, Pakistan <br />
+                  PinCode: 131103
+                </address>
+                <a
+                  href="tel: +92 3328578135"
+                  className="mt-3 d-block md-1 text-white"
+                >
+                  +92 3328578135
+                </a>
+                <a
+                  href="mailto: taimour.rkt@gmail.com"
+                  className="mt-2 d-block md-0 text-white"
+                >
+                  taimour.rkt@gmail.com
+                </a>
+              </div>
+              <div className="social-icons d-flex align-items-center gap-30 mt-4 ">
+                <a className="text-white" href=""><BsLinkedin className=" fs-5"/>  </a>
+                <a className="text-white" href=""><BsInstagram className=" fs-5"/> </a>
+                <a className="text-white" href=""><BsGithub className=" fs-5"/> </a>
+                <a className="text-white" href=""><BsYoutube className=" fs-5"/> </a>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
 
-    <div>Footer</div>
-    )
-}
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1">
+                  Terms and Conditions
+                </Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>{" "}
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">About Us</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div>
+                <div className="footer-links d-flex flex-column">
+                  <Link className="text-white py-2 mb-1">Laptops</Link>
+                  <Link className="text-white py-2 mb-1">Headphones</Link>
+                  <Link className="text-white py-2 mb-1">Tablets</Link>
+                  <Link className="text-white py-2 mb-1">Watch</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()} Made with ❤
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
