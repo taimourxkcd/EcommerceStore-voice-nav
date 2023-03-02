3353155946d818584d1bced27d31a87969f55206
taimourz <taimour.rkt@gmail.com>
2023-03-02 16:25:11 +0500

    home page middle section part 1 complete

--- app/javascript/src/pages/Home.js
+++ app/javascript/src/pages/Home.js
@@ -1,11 +1,96 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative">
+                <img
+                  src={require("../../Public/images/main-banner-1.jpg")}
+                  alt="main banner"
+                  className="img-fluid round-3"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUPERCHARGED FOR PROS</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>From PKR999.00 or PKR42.62/month</p>
+                  <Link className="button">BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap justify-content-between gap-7 align-items-center">
+                <div className="small-banner position-relative fix-size">
+                  <img
+                    src={require("../../Public/images/catbanner-01.jpg")}
+                    alt="main banner"
+                    className="img-fluid round-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Best Sale</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>
+                      From PKR999 <br />
+                      to PKR42.62/mo
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative ">
+                  <img
+                    src={require("../../Public/images/catbanner-02.jpg")}
+                    alt="main banner"
+                    className="img-fluid round-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>Buy iPad Air</h5>
+                    <p>
+                      From PKR999 <br />
+                      to PKR42.62/mo
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src={require("../../Public/images/catbanner-03.jpg")}
+                    alt="main banner"
+                    className="img-fluid round-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>New Arrival</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>
+                      From PKR999 <br />
+                      to PKR42.62/mo
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src={require("../../Public/images/catbanner-04.jpg")}
+                    alt="main banner"
+                    className="img-fluid round-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Best Sale</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>
+                      From PKR999 <br />
+                      to PKR42.62/mo
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
