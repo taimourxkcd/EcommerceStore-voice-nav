4cc26fe6c1e925bc9b13a99f27009e1219fc3e4e
taimourz <taimour.rkt@gmail.com>
2023-03-05 15:38:46 +0500

    login-page complete

--- app/javascript/src/pages/Login.js
+++ app/javascript/src/pages/Login.js
@@ -1,14 +1,51 @@
-import React from 'react'
+import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <>
-      <Meta title={"Our Store"} />
-      <BreadCrumb title=" Our Store" />
+      <Meta title={"Login"} />
+      <BreadCrumb title="Login" />
+
+      <div className="login-wrapper py-5 home-wrapper-2">
+        <div className="row"></div>
+        <div className="col-12">
+          <div className="auth-card">
+            <h3 className="text-center mb-3">Login</h3>
+            <form action="" className="d-flex flex-column gap-15">
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className="form-control"
+                />
+              </div>
+              <div className="mt-3">
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className="form-control"
+                />
+              </div>
+              <div>
+                <Link to='/forgot-password'>Forgot Password?</Link>
+
+                <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
+                    <button className="button border-0">Login</button>
+                    <Link className="button signup">Sign up</Link>
+                </div>
+
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
-}
+};
 
-export default Login
+export default Login;
