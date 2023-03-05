bee0a8af295d80455a7fb609ffffb7823e25507b
taimourz <taimour.rkt@gmail.com>
2023-03-05 16:29:14 +0500

    reset password page initial setup

---
+++ app/javascript/src/pages/Resetpassword.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+
+const Resetpassword = () => {
+  return (
+    <>
+      <Meta title={"Reset Password"} />
+      <BreadCrumb title="Reset Password" />
+    </>
+  );
+}
+
+export default Resetpassword
