6ca4b21c56f62a78130eca0ad639d4e1986707b0
taimourz <taimour.rkt@gmail.com>
2023-03-05 16:10:32 +0500

    signup page initial setup

--- app/javascript/src/pages/Signup.js
+++ app/javascript/src/pages/Signup.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { Link } from 'react-router-dom';
+
+
 
 const Signup = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Meta title={"SignUp"} />
+      <BreadCrumb title="Sign up" />
+
+     
+    </>
+  );
 }
 
 export default Signup
