cf962752f7abe6788eedc8ca455910504af92374
taimourz <taimour.rkt@gmail.com>
2023-03-05 15:51:49 +0500

    initial setup forgot password page

--- app/javascript/src/pages/ForgotPassword.js
+++ app/javascript/src/pages/ForgotPassword.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { Link } from "react-router-dom";
 
 const ForgotPassword = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Meta title={"Forgot Password"} />
+      <BreadCrumb title="Forgot Password" />
 
-export default ForgotPassword
+      <div className="login-wrapper py-5 home-wrapper-2">
+        <div className="row"></div>
+        <div className="col-12">
+          <div className="auth-card">
+            <h3 className="text-center mb-3">Login</h3>
+            <form action="" className="d-flex flex-column gap-15">
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className="form-control"
+                />
+              </div>
+
+              <div>
+                <Link>Forgot Password?</Link>
+
+                <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
+                  <button className="button border-0">Login</button>
+                  <Link className="button signup">Sign up</Link>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ForgotPassword;
