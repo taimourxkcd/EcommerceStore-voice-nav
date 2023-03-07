3ee6149911881badd0e3cc387423599c7ca050a9
taimourz <taimour.rkt@gmail.com>
2023-03-07 10:40:45 +0500

    working..

--- app/javascript/src/pages/SingleProduct.js
+++ app/javascript/src/pages/SingleProduct.js
@@ -3,18 +3,30 @@ import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
 import ProductCard from "../components/ProductCard";
 import ReactStars from "react-rating-stars-component";
+import ReactImageZoom from 'react-image-zoom'
 
 const SingleProduct = () => {
+ 
+  const props = {width: 400, height: 250, zoomWidth: 500, img: "1.jpg"}
+
   const [orderedProduct, setorderedProduct] = useState(true);
+
+
 
   return (
     <>
       <Meta title={"Single Product"} />
       <BreadCrumb title=" Single Product" />
       <div className="main-product-wrapper py-5 home-wrapper-2">
-        <div className="container-xxl">
+        <div className="container-xxl ">
           <div className="row">
-            <div className="col-6"></div>
+            <div className="col-6">
+              <div className="main-product-image">
+                <div>
+                   <ReactImageZoom {...props}/>
+                   </div>
+              </div>
+            </div>
             <div className="col-6"></div>
           </div>
         </div>
