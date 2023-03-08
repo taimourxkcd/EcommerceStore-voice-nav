6c1c7eb7416fdfff294993049cf2189a54e6986e
taimourz <taimour.rkt@gmail.com>
2023-03-08 23:00:49 +0500

    half portion of cart page done

--- app/javascript/src/pages/Cart.js
+++ app/javascript/src/pages/Cart.js
@@ -1,13 +1,103 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
-
+import watchImg from "../../Public/images/watch.jpg";
+import { AiFillDelete } from "react-icons/ai";
 
 const Cart = () => {
   return (
     <>
       <Meta title={"Cart"} />
       <BreadCrumb title="Cart" />
+
+      <section className="cart-wrapper home-wrapper-2 py-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="cart-header py-3 d-flex justify-content-between align-items-center">
+                <h4 className="cart-col-1">Product</h4>
+                <h4 className="cart-col-2">Price</h4>
+                <h4 className="cart-col-3">Quantity</h4>
+                <h4 className="cart-col-4">Total</h4>
+              </div>
+              <div className="cart-data py-3 mb-2 d-flex justify-content-between align-items-center">
+                <div className="cart-col-1 gap-15 d-flex algin-items-center">
+                  <div className="w-25">
+                    <img
+                      src={watchImg}
+                      alt="product img"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div>
+                    <p>dafasdfas</p>
+                    <p>Size: fasdf</p>
+                    <p>Color: fadsf</p>
+                  </div>
+                </div>
+                <div className="cart-col-2">
+                  <h5 className="price">$ 100</h5>
+                </div>
+                <div className="cart-col-3 d-flex align-items-center gap-15">
+                  <div>
+                    <input
+                      className="form-control"
+                      type="number"
+                      min={1}
+                      max={10}
+                      name=""
+                      id=""
+                    />
+                  </div>
+                  <div>
+                    <AiFillDelete className="text-danger" />
+                  </div>
+                </div>
+                <div className="cart-col-4">
+                  <h5 className="price">$ 100</h5>
+                </div>
+              </div>
+              <div className="cart-data py-3 mb-2 d-flex justify-content-between align-items-center">
+                <div className="cart-col-1 gap-15 d-flex algin-items-center">
+                  <div className="w-25">
+                    <img
+                      src={watchImg}
+                      alt="product img"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div>
+                    <p>dafasdfas</p>
+                    <p>Size: fasdf</p>
+                    <p>Color: fadsf</p>
+                  </div>
+                </div>
+                <div className="cart-col-2">
+                  <h5 className="price">$ 100</h5>
+                </div>
+                <div className="cart-col-3 d-flex align-items-center gap-15">
+                  <div>
+                    <input
+                      className="form-control"
+                      type="number"
+                      min={1}
+                      max={10}
+                      name=""
+                      id=""
+                    />
+                  </div>
+                  <div>
+                    <AiFillDelete className="text-danger" />
+                  </div>
+                </div>
+                <div className="cart-col-4">
+                  <h5 className="price">$ 100</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
