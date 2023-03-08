1377b3d34bdd7ba40a66f993e8c349729418274a
taimourz <taimour.rkt@gmail.com>
2023-03-08 23:21:49 +0500

    initial setup checkout page

---
+++ app/javascript/src/pages/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BreadCrumb from '../components/BreadCrumb'
+import Meta from '../components/Meta'
+
+const Checkout = () => {
+  return (
+    <>
+      <Meta title={"Checkout"} />
+      <BreadCrumb title=" Checkout" />
+    </>
+  );
+}
+
+export default Checkout
