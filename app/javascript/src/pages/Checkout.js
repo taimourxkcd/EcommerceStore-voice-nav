e56b473bee7d22e77e61f841ce7f6607c71b2510
taimourz <taimour.rkt@gmail.com>
2023-03-09 15:03:38 +0500

    form portion checkout page done

--- app/javascript/src/pages/Checkout.js
+++ app/javascript/src/pages/Checkout.js
@@ -1,14 +1,124 @@
-import React from 'react'
-import BreadCrumb from '../components/BreadCrumb'
-import Meta from '../components/Meta'
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { Link } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
 
 const Checkout = () => {
   return (
     <>
       <Meta title={"Checkout"} />
       <BreadCrumb title=" Checkout" />
+      <div className="checkout-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-7">
+              <div className="checkout-left-data">
+                <h3 className="website-name">Online Store</h3>
+                <nav
+                  style={{ "--bs-breadcrumb-divider": ">" }}
+                  aria-label="breadcrumb"
+                >
+                  <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                      <Link to="/cart" className="text-dark">Cart</Link>
+                    </li>
+                    &nbsp; /
+                    <li className="breadcrumb-item active" aria-current="page">
+                      Information
+                    </li>
+                    &nbsp;/
+                    <li className="breadcrumb-item active"> Shipping</li>
+                    &nbsp; /
+                    <li className="breadcrumb-item active" aria-current="page">
+                      Payment
+                    </li>
+                  </ol>
+                </nav>
+                <h4 className="title">Contact information</h4>
+                <p className="user-details">
+                  Taimour Afzal (taimour.rkt@gmail.com)
+                </p>
+                <form
+                  action=""
+                  className="d-flex gap-15 flex-wrap justify-content-between"
+                >
+                  <div className="w-100">
+                    <select name="" className="form-control form-select" id="">
+                      <option value="" selected disabled>
+                        Select Country
+                      </option>
+                    </select>
+                  </div>
+                  <div className="flex-grow-1">
+                    <input
+                      type="text"
+                      placeholder="First Name"
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="flex-grow-1">
+                    <input
+                      type="text"
+                      placeholder="Last Name"
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="w-100">
+                    <input
+                      type="text"
+                      placeholder="Address"
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="w-100">
+                    <input
+                      type="text"
+                      placeholder="Apartment Suite etc"
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="flex-grow-1">
+                    <input
+                      type="text"
+                      placeholder="City"
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="flex-grow-1">
+                    <select name="" className="form-control form-select" id="">
+                      <option value="" selected disabled>
+                        Select Province
+                      </option>
+                    </select>
+                  </div>
+                  <div className="flex-grow-1">
+                    <input
+                      type="text"
+                      placeholder="Zip Code"
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="w-100">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <Link to="/cart" className="text-dark">
+                        <BiArrowBack className="me-2" />
+                        Return to Cart
+                      </Link>
+                      <Link to="/cart" className="button">
+                        Continue to Shipping
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="col-5"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
-}
+};
 
-export default Checkout
+export default Checkout;
