7e27d9dd6ea7a992eab863be6b7dd803d628e74b
taimourz <taimour.rkt@gmail.com>
2023-05-23 16:49:24 +0500

    get popular products from db

--- app/javascript/src/components/ProductCard.js
+++ app/javascript/src/components/ProductCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactStars from "react-rating-stars-component";
 import { Link, useLocation } from "react-router-dom";
 import watchImg from "../../Public/images/watch.jpg"
@@ -9,141 +9,83 @@ import viewImg from "../../Public/images/view.svg";
 import prodcompareImg from "../../Public/images/prodcompare.svg";
 
 const ProductCard = (props) => {
-  const { grid } = props;
+ const { grid } = props;
+ const [product, setProduct] = useState(null);
+ const location = useLocation();
 
-  let location = useLocation();
+ useEffect(() => {
+   const fetchProduct = async () => {
+     try {
+       const response = await fetch("http://localhost:3000/api/v1/products/28");
+       const data = await response.json();
+       setProduct(data);
+     } catch (error) {
+       console.error("Error fetching product:", error);
+     }
+   };
+
+   fetchProduct();
+ }, []);
+
+ if (!product) {
+   return null; // Render a loading state or placeholder if product data is not available yet
+ }
+
+ const imgPath = product.image_path;
 
   return (
-    <>
-      <div
-        className={` ${
-          location.pathname === "/store" ? `gr-${grid}` : "col-3"
-        }`}
-      >
-        <Link to=":id" className="product-card position-relative">
-          <div className="wishlist-icon position-absolute">
+    <div className={`col-3`}>
+      <Link to=":id" className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+          <button className="border-0 bg-transparent">
+            <img src={wishImg} alt="wish" />
+          </button>
+        </div>
+        <div className="product-image">
+          <img
+            src={product.image_path}
+            alt="product image"
+            className="img-fluid"
+          />
+          <img
+            src={product.image_path2}
+            alt="product image"
+            className="img-fluid"
+          />
+        </div>
+        <div className="product-details">
+          <h6 className="brand">{product.brand}</h6>
+          <h5 className="product-title">{product.title}</h5>
+          <ReactStars
+            count={5}
+            size={24}
+            value={3}
+            edit={false}
+            activeColor="#ffc700"
+          ></ReactStars>
+          <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nihil
+            dolor alias dignissimos soluta numquam cumque ratione, voluptates
+            exercitationem. Dolor odit rerum ad laborum, quis dolore corrupti
+            incidunt. Sapiente, odit!
+          </p>
+          <p className="price">{product.price}</p>
+        </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
             <button className="border-0 bg-transparent">
-              <img src={wishImg} alt="wish" />
+              <img src={addcartImg} alt="add-cart" />
+            </button>
+            <button className="border-0 bg-transparent">
+              <img src={viewImg} alt="view" />
+            </button>
+            <button className="border-0 bg-transparent">
+              <img src={prodcompareImg} alt="compare" />
             </button>
           </div>
-          <div className="product-image">
-            <img
-              src={watchImg}
-              alt="product image"
-              className="img-fluid"
-            />
-            <img
-              src={tvImg}
-              alt="product image"
-              className="img-fluid"
-            />
-          </div>
-          <div className="product-details">
-            <h6 className="brand">Havels</h6>
-            <h5 className="product-title">
-              Kids headphones 10 pack multi colored for students
-            </h5>
-            <ReactStars
-              count={5}
-              size={24}
-              value={3}
-              edit={false}
-              activeColor="#ffc700"
-            ></ReactStars>
-            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nihil
-              dolor alias dignissimos soluta numquam cumque ratione, voluptates
-              exercitationem. Dolor odit rerum ad laborum, quis dolore corrupti
-              incidunt. Sapiente, odit!
-            </p>
-            <p className="price">PKR 100</p>
-          </div>
-          <div className="action-bar position-absolute">
-            <div className="d-flex flex-column gap-15">
-              <button className="border-0 bg-transparent">
-                <img
-                  src={addcartImg}
-                  alt="add-cart"
-                />
-              </button>
-              <button className="border-0 bg-transparent">
-                <img src={viewImg} alt="view" />
-              </button>
-              <button className="border-0 bg-transparent">
-                <img
-                  src={prodcompareImg}
-                  alt="compare"
-                />
-              </button>
-            </div>
-          </div>
-        </Link>
-      </div>
-      <div
-        className={` ${
-          location.pathname === "/store" ? `gr-${grid}` : "col-3"
-        }`}
-      >
-        <Link className="product-card position-relative">
-          <div className="wishlist-icon position-absolute">
-            <button className="border-0 bg-transparent">
-              <img src={wishImg} alt="wish" />
-            </button>
-          </div>
-          <div className="product-image">
-            <img
-              src={watchImg}
-              alt="product image"
-              className="img-fluid"
-            />
-            <img
-              src={tvImg}
-              alt="product image"
-              className="img-fluid"
-            />
-          </div>
-          <div className="product-details">
-            <h6 className="brand">Havels</h6>
-            <h5 className="product-title">
-              Kids headphones 10 pack multi colored for students
-            </h5>
-            <ReactStars
-              count={5}
-              size={24}
-              value={3}
-              edit={false}
-              activeColor="#ffc700"
-            ></ReactStars>
-            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nihil
-              dolor alias dignissimos soluta numquam cumque ratione, voluptates
-              exercitationem. Dolor odit rerum ad laborum, quis dolore corrupti
-              incidunt. Sapiente, odit!
-            </p>
-            <p className="price">PKR 100</p>
-          </div>
-          <div className="action-bar position-absolute">
-            <div className="d-flex flex-column gap-15">
-              <button className="border-0 bg-transparent">
-                <img
-                  src={addcartImg}
-                  alt="add-cart"
-                />
-              </button>
-              <button className="border-0 bg-transparent">
-                <img src={viewImg} alt="view" />
-              </button>
-              <button className="border-0 bg-transparent">
-                <img
-                  src={prodcompareImg}
-                  alt="compare"
-                />
-              </button>
-            </div>
-          </div>
-        </Link>
-      </div>
-    </>
+        </div>
+      </Link>
+    </div>
   );
 };
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_121246) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_113310) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -115,6 +115,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_121246) do
     t.integer "brand_id"
     t.integer "rating"
     t.string "image_path"
+    t.string "brand"
+    t.string "image_path2"
+    t.string "image_path3"
+    t.string "image_path4"
     t.index ["slug"], name: "index_products_on_slug", unique: true
     t.check_constraint "color = ANY (ARRAY[0, 1, 2])", name: "check_color"
   end
