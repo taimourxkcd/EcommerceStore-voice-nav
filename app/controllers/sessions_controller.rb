7101d37cf41258317e14f2df38563e0dd29c401b
taimourz <taimour.rkt@gmail.com>
2023-05-28 21:56:07 +0500

    completely get rid of concerns

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,4 @@
 class SessionsController < ApplicationController
-  # include CurrentUserConcern
-
   def create
     user = User.find_by(email: params[:user][:email])
     if user && user.authenticate(params[:user][:password])
